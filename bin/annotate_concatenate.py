424a4705f76b27e25278be65b36464e897fd11d8
pennycuda <141153875+pennycuda@users.noreply.github.com>
2024-04-05 10:43:14 -0400

    Give pandas an str dtype

--- bin/annotate_concatenate.py
+++ bin/annotate_concatenate.py
@@ -76,15 +76,18 @@ def annotate_file(filtered_file: Path, unfiltered_file: Path, tissue_type:str) -
 
     unfiltered_adata.uns['annotation_metadata'] = filtered_adata.uns['annotation_metadata'] if \
         'annotation_metadata' in filtered_adata.uns.keys() else {'is_annotated':False}
+    print(unfiltered_adata.uns['annotation_metadata'])
     unfiltered_copy = unfiltered_adata.copy()
     unfiltered_copy.obs['barcode'] = unfiltered_adata.obs.index
     unfiltered_copy.obs['dataset'] = data_set_dir
     unfiltered_copy.obs['organ'] = tissue_type
     unfiltered_copy.obs['modality'] = 'rna'
-    unfiltered_copy.obs['dataset_leiden'] = pd.Series(index=unfiltered_copy.obs.index)
-
+    unfiltered_copy.obs['dataset_leiden'] = pd.Series(index=unfiltered_copy.obs.index, dtype=str)
+    unfiltered_copy.uns['annotation_metadata'] = filtered_adata.uns['annotation_metadata'] if \
+        'annotation_metadata' in filtered_adata.uns.keys() else {'is_annotated':False}
+    print(unfiltered_copy.uns_keys())
     for field in annotation_fields:
-        unfiltered_copy.obs[field] = pd.Series(index=unfiltered_copy.obs.index)
+        unfiltered_copy.obs[field] = pd.Series(index=unfiltered_copy.obs.index, dtype=str)
 
     for barcode in unfiltered_copy.obs.index:
         dataset_clusters_and_cell_types = get_dataset_cluster_and_cell_type_if_present(barcode, filtered_adata, data_set_dir)
@@ -96,8 +99,8 @@ def annotate_file(filtered_file: Path, unfiltered_file: Path, tissue_type:str) -
     unfiltered_copy.obs.set_index("cell_id", drop=True, inplace=True)
 
     unfiltered_copy = map_gene_ids(unfiltered_copy)
-
-    return unfiltered_copy.copy()
+    print(unfiltered_copy.uns_keys())
+    return unfiltered_copy
 
 def read_gene_mapping() -> Dict[str, str]:
     """
@@ -138,6 +141,8 @@ def main(data_directory:Path, uuids_file: Path, tissue:str=None):
     # Load files
     file_pairs = [find_file_pairs(directory) for directory in directories]
     adatas = [annotate_file(file_pair[0],file_pair[1], tissue) for file_pair in file_pairs]
+    for adata in adatas:
+        print(adata.uns_keys())
     annotation_metadata = {adata.obs.dataset.iloc[0]:adata.uns['annotation_metadata'] for adata in adatas}
     adata = anndata.concat(adatas)
     adata.uns['annotation_metadata'] = annotation_metadata
