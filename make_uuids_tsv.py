ace8e2aa33c81b7c2053dd9d9d3f93d9ba8c1ea1
pennycuda <141153875+pennycuda@users.noreply.github.com>
2024-04-15 11:26:17 -0400

    Get hubmap id from the search query

--- make_uuids_tsv.py
+++ make_uuids_tsv.py
@@ -10,7 +10,9 @@ def get_uuids(organ_name_mapping:dict, organ:str=None):
   organ_code_mapping = {organ_name_mapping[key]:key for key in organ_name_mapping}
   url = f'https://search.api.hubmapconsortium.org/v3/param-search/datasets?dataset_type=RNAseq&origin_samples.organ={organ_code_mapping[organ]}' if organ else \
     'https://search.api.hubmapconsortium.org/v3/param-search/datasets?dataset_type=RNAseq'
-  return [item['uuid'] for item in requests.get(url).json()]
+  uuids = [item['uuid'] for item in requests.get(url).json()]
+  hubmap_ids = [item['hubmap_id'] for item in requests.get(url).json()]
+  return uuids, hubmap_ids
 
 def main(tissue_type: str):
     organ_dict = yaml.load(open(organ_types_yaml_file), Loader=yaml.BaseLoader)
@@ -21,9 +23,10 @@ def main(tissue_type: str):
         print(f"Tissue {tissue_type} not found ")
         tissue_type = None
 
-    uuids_list = get_uuids(organ_dict, tissue_type)
+    uuids_list, hubmap_ids_list = get_uuids(organ_dict, tissue_type)
     uuids_df = pd.DataFrame()
     uuids_df['uuid'] = pd.Series(uuids_list)
+    uuids_df['hubmap_id'] = pd.Series(hubmap_ids_list)
     output_file_name = f"{tissue_type.lower()}.tsv" if tissue_type else "rna.tsv"
     uuids_df.to_csv(output_file_name, sep='\t')
 
