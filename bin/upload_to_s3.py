464e3c77244d4bff963be1b92f0f3098cd7a5a9a
pennycuda <141153875+pennycuda@users.noreply.github.com>
2024-05-03 17:22:25 -0400

    make tissue folder

--- bin/upload_to_s3.py
+++ bin/upload_to_s3.py
@@ -7,21 +7,21 @@ import awswrangler as wr
 import boto3
 
 
-def upload_file_to_s3(path_to_file, boto_session):
-    remote_path = f"s3://hubmap-data-products/{path_to_file.name}"
+def upload_file_to_s3(path_to_file, tissue, boto_session):
+    remote_path = f"s3://hubmap-data-products/{tissue}/{path_to_file.name}"
     with open(path_to_file, "rb") as local_f:
         wr.s3.upload(local_file=local_f, path=remote_path, boto3_session=boto_session)
 
 
-def upload_files_to_s3(paths_to_files, access_key_id, secret_access_key):
+def upload_files_to_s3(paths_to_files, tissue, access_key_id, secret_access_key):
     boto_session = boto3.Session(access_key_id, secret_access_key)
     for path in paths_to_files:
-        upload_file_to_s3(path, boto_session)
+        upload_file_to_s3(path, tissue, boto_session)
 
 
-def main(raw_h5ad, processed_h5ad, umap, access_key_id, secret_access_key):
+def main(raw_h5ad, processed_h5ad, umap, tissue, access_key_id, secret_access_key):
     files = [raw_h5ad, processed_h5ad, umap]
-    upload_files_to_s3(files, access_key_id, secret_access_key)
+    upload_files_to_s3(files, tissue, access_key_id, secret_access_key)
     f = open("finished.txt", "w")
     f.write("Finished da pipeline")
     f.close()
@@ -32,6 +32,7 @@ if __name__ == "__main__":
     p.add_argument("raw_h5ad_file", type=Path)
     p.add_argument("processed_h5ad_file", type=Path)
     p.add_argument("umap_png", type=Path, nargs="?")
+    p.add_argument("tissue", type=str)
     p.add_argument("access_key_id", type=str)
     p.add_argument("secret_access_key", type=str)
 
@@ -40,6 +41,7 @@ if __name__ == "__main__":
         args.raw_h5ad_file,
         args.processed_h5ad_file,
         args.umap_png,
+        args.tissue,
         args.access_key_id,
         args.secret_access_key,
     )
