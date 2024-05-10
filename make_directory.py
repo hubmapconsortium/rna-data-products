26a6489f8a52bbb48764fc16f82b7d900d059930
pennycuda <141153875+pennycuda@users.noreply.github.com>
2024-05-10 12:12:57 -0400

    Skip ancient rna-seq datasets

--- make_directory.py
+++ make_directory.py
@@ -19,8 +19,8 @@ def find_files(directory, patterns):
 
 
 def find_file_pairs(directory):
-    filtered_patterns = ["cluster_marker_genes.h5ad", "secondary_analysis.h5ad"]
-    unfiltered_patterns = ["out.h5ad", "expr.h5ad"]
+    filtered_patterns = ["secondary_analysis.h5ad"]
+    unfiltered_patterns = ["expr.h5ad"]
     filtered_file = find_files(directory, filtered_patterns)
     unfiltered_file = find_files(directory, unfiltered_patterns)
     return filtered_file, unfiltered_file
