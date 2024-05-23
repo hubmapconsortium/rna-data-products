3f25258b68eeb11d00f5632e0ff87f117d711996
pennycuda <141153875+pennycuda@users.noreply.github.com>
2024-05-23 10:14:07 -0400

    typo

--- bin/azimuth.R
+++ bin/azimuth.R
@@ -194,7 +194,7 @@ if (tissue %in% c("RK", "LK", "RL", "LL", "HT")) {
 
   # add reference-guided UMAP to anndata object
   umap.new <- matrix(c(df$V1, df$V2), ncol=2)
-  raw_h5ad$obsm$X_umap_proj <- query[["proj.umap"]]
+  raw_h5ad$obsm$X_umap_proj <- umap.new
 
   # save modified secondary_analysis.h5ad matrix to a new annotated equivalent
   write_h5ad(raw_h5ad, secondary.analysis.path) 
