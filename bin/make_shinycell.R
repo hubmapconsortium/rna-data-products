1b25fd85aaeb1a3bdb9aa7e1b1c9d1e604d0c649
Sarah <sschultz@cs.cmu.edu>
2024-06-04 14:17:39 -0400

    Add step for creating shinycell app

---
+++ bin/make_shinycell.R
@@ -0,0 +1,15 @@
+library(ShinyCell)
+
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) != 2) {
+  cat("Usage: Rscript make_shinycell.R <adata_object> <ref>\n")
+  quit(status = 1)
+}
+inpFile <- args[1]
+tissue <- args[2]
+
+
+scConf = createConfig(inpFile)
+dir = sprintf("../shinyApps/% s/", tissue)
+title = sprintf("Shiny Cell h5ad % s", tissue)
+makeShinyApp(inpFile, scConf, shiny.dir = dir, shiny.title = title) 
