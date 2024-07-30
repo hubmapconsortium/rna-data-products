410431d226e85de89443576f58d577cec10e45c0
pennycuda <141153875+pennycuda@users.noreply.github.com>
2024-07-30 13:19:52 -0400

    skip a cell

--- bin/mtx_to_seurat.R
+++ bin/mtx_to_seurat.R
@@ -10,7 +10,7 @@ barcodes_file <- args[3]
 # Convert MTX to Seurat object
 expression_matrix <- ReadMtx(
 mtx = mtx_file, features = features_file, cells = barcodes_file,
-cell.column = 1, feature.column = 1, mtx.transpose = TRUE
+cell.column = 1, feature.column = 1, mtx.transpose = TRUE, skip.cell = 1
 )
 seurat_object <- CreateSeuratObject(counts = expression_matrix)
 
