a6bdfcc8afa1ba0a1ff02b0b1e2c7c7dae0b5cf1
Sarah Schultz <sschultz@vm010.hive.psc.edu>
2024-08-27 10:04:50 -0400

    Use hugo symbol as gene name (if available)

--- bin/make_shinycell.R
+++ bin/make_shinycell.R
@@ -15,11 +15,37 @@ inpFile <- args[1]
 tissue <- args[2]
 metadata_file <- args[3]
 
+mapping_file <- "/opt/ensembl_to_symbol.json"
+
 options(future.globals.maxSize = 8000 * 1024^2)
 
 sce <- readH5AD(inpFile, use_hdf5 = TRUE)
 
 sce <- logNormCounts(sce, assay.type="unscaled")
+
+#read the mapping file
+hugoMapping <- fromJSON(file=mapping_file)
+
+#get the ensemble ids
+ensIds <- rownames(sce)
+
+#convert ensemble ids to hugo sybmols
+hugos<-unname(hugoMapping[ensIds])
+
+#replace null hugos with the ensId
+for (i in 1:length(hugos)){
+  h <- unlist(hugos[i])
+  if(is.null(h)){
+    hugos[i]<-ensIds[i]}
+  else{
+    hugos[i]<-h
+  }
+}
+#convert to 1-d list
+hugos<-unlist(hugos)
+
+#replace the rownames of the sce
+rownames(sce)<-hugos
 
 tryCatch({
   scConf = createConfig(sce)
