f4511f51a98c8912db0443cfcfa4f1c180f6b7d8
seschultz <sschult1@andrew.cmu.edu>
2025-01-02 19:04:13 -0500

    Make sure no spaces in cell_type_counts, black

--- bin/prep_to_convert.py
+++ bin/prep_to_convert.py
@@ -5,18 +5,28 @@ from argparse import ArgumentParser
 from pathlib import Path
 import anndata
 
+
 def main(processed_h5ad_file: Path):
     adata = anndata.read_h5ad(processed_h5ad_file)
-    #CWL can't handle spaces in file names, so replace spaces before converting to zarr
-    unds_names=[]
-    for name in adata.obs['predicted_label'].cat.categories:
+    # CWL can't handle spaces in file names, so replace spaces before converting to zarr
+    unds_names = []
+    for name in adata.obs["predicted_label"].cat.categories:
         unds_names.append(name.replace(" ", "_"))
-    adata.rename_categories('predicted_label', unds_names)
-    #save where required for use with anndata-to-ui
+    adata.rename_categories("predicted_label", unds_names)
+    # have to do this one separately
+    cell_types=[]
+    for cell_type in adata.uns["cell_type_counts"]:
+        cell_types.append(cell_type)
+    for cell_type in cell_types:
+        adata.uns["cell_type_counts"][cell_type.replace(" ", "_")] = adata.uns[
+            "cell_type_counts"
+        ].pop(cell_type)
+    # save where required for use with anndata-to-ui
     os.mkdir("./processed_dir")
     adata.write_h5ad("./processed_dir/secondary_analysis.h5ad")
-    #shutil.copy(processed_h5ad_file, "./processed_dir/secondary_analysis.h5ad")
-    
+    # shutil.copy(processed_h5ad_file, "./processed_dir/secondary_analysis.h5ad")
+
+
 if __name__ == "__main__":
     p = ArgumentParser()
     p.add_argument("--processed_h5ad_file", type=Path)
