229d9f03f6ce613edf73d6f64c2dedb0426ea923
seschultz <sschult1@andrew.cmu.edu>
2025-01-07 10:50:30 -0500

    Fix directory upload

--- bin/upload_zarr_to_s3.py
+++ bin/upload_zarr_to_s3.py
@@ -9,6 +9,7 @@ import zarr
 import json
 #import awswrangler
 import awscli
+import shutil
 
 def set_access_keys(access_key_id: str, secret_access_key: str):
     os.system(f"aws configure set aws_access_key_id \"{access_key_id}\"")
@@ -21,6 +22,16 @@ def upload_file_to_s3(local_file: Path, uuid: str):
     file_name = os.path.basename(local_file)
     os.system(f"aws s3 cp \"{local_file}\" \"{bucket_path}{file_name}\" --expected-size \"{file_size}\"")
 
+def upload_dir_to_s3(local_dir: Path, uuid: str):
+    bucket_path = f"s3://hubmap-data-products/{uuid}/"
+    dir_name = os.path.basename(local_dir)
+    for root,dirs,files in os.walk(local_dir, topdown=False):
+        for local_file in files:
+            #file_size = os.path.getsize(local_file)
+            file_name = os.path.join(root, local_file)
+            print(file_name)
+            file_size = os.path.getsize(file_name)#(f"{local_dir}/{local_file}")
+            os.system(f"aws s3 cp \"{file_name}\" \"{bucket_path}{dir_name}/{file_name}\" --expected-size \"{file_size}\"")
 
 def main(zarr_dir: Path, metadata_file: Path, access_key_id: str, secret_access_key: str):
     #stripped_filename = h5ad.removesuffix(".h5ad")
@@ -30,10 +41,10 @@ def main(zarr_dir: Path, metadata_file: Path, access_key_id: str, secret_access_
     with open(metadata_file, 'r') as file:
         data = json.load(file)
     uuid = data['Data Product UUID']
-    zarr_file = f"{zarr_dir}/secondary_analysis.zarr"
+    zarr_file = shutil.copytree(f"{zarr_dir}/secondary_analysis.zarr","./zarr/secondary_analysis.zarr")
     zarr.consolidate_metadata(zarr_file)
     set_access_keys(access_key_id, secret_access_key)
-    upload_file_to_s3(zarr_file, uuid)
+    upload_dir_to_s3(zarr_file, uuid)
     f = open("finished_zarr.txt", "w")
     f.write("zarr uploaded")
     f.close()
