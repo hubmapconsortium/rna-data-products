f4a75b31b1c8545579db0b7dbb3189147073da04
pennycuda <141153875+pennycuda@users.noreply.github.com>
2026-07-13 10:27:26 -0400

    update upload scripts

--- upload_to_vm.py
+++ upload_to_vm.py
@@ -8,7 +8,7 @@ from pathlib import Path
 def get_uuid(data_product_metadata):
     with open(data_product_metadata, "r") as json_file:
         metadata = json.load(json_file)
-    uuid = metadata["Data Product UUID"]
+    uuid = metadata["Integrated Map UUID"]
     return uuid
 
 
